420c5ade9da9f3e010e2943eabdafc651f8b7774
Dave Funk <davefunk135@gmail.com>
2018-05-17 22:14:20 -0700

    image added to landing

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,8 +1,9 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import snoo from './images/snoo.png'
 
 const Landing =() => {
-  return <Link to='/home'><div>Welcome</div></Link>;
+  return <Link to='/home'><img className='enter' src={snoo} alt='enter'/></Link>;
 };
 
 export default Landing;
