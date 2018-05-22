9d3f8e285b9ec494685aed5bdd1ad8bc664cde44
Dave Funk <davefunk135@gmail.com>
2018-05-21 17:43:47 -0700

    fixed search form bug, switch case implemented in leiu of ternary expressions

--- client/src/components/SearchForm/SearchForm.js
+++ client/src/components/SearchForm/SearchForm.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const SearchForm = props => (
-  // <div className="form-group">
+  <div className="form-group">
     <input className="form-control" {...props} />
-  // </div>
+  </div>
 );
 
 export default SearchForm;
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,74 +1,100 @@
 import React, { Component } from "react";
-import SearchForm from '../../components/SearchForm/SearchForm'
-import Snoo from '../../utils/Snoo'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux';
-import * as postActionCreators from '../../actions/postActions';
-import SearchContainer from '../../components/SearchContainer/SearchContainer'
+import SearchForm from "../../components/SearchForm/SearchForm";
+import Snoo from "../../utils/Snoo";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as postActionCreators from "../../actions/postActions";
+import SearchContainer from "../../components/SearchContainer/SearchContainer";
 import Card from "../../components/Card/Card";
 
 class Search extends Component {
-    state = {
-        search: '',
-        empty: 'There are no posts here',
-    }
-    handleInputChange = event => {
-        const { id, value } = event.target;
-        this.setState({
-            [id]: value
-        });
-        console.log(this.state)
-    };
+  postFilter = posts => {
+    return <div>hello</div>;
+  };
 
-    handleSearch = event => {
-        event.preventDefault();
-        if (this.state.search) {
-            Snoo.snooScrape(this.state.search).then(res => {
-                const { postActions } = this.props;
-                postActions.postsToProps(res)
-            })
-        }
-    };
-    render() {
-        return (
-            <SearchContainer>
-                <div className='search-form-container'>
-                    <p className='search-text'>Please Search for a subreddit to return its contents!</p>
-                    <div className='search-form'>
-                        <SearchForm
-                            id='search'
-                            onChange={this.handleInputChange}
-                        />
-                    </div>
-                    <button className='search-button' onClick={this.handleSearch}>Search</button>
-                </div>
+  state = {
+    search: "",
+    empty: "There are no posts here"
+  };
+  handleInputChange = event => {
+    const { id, value } = event.target;
+    this.setState({
+      [id]: value
+    });
+    console.log(this.state);
+  };
 
-                {this.props.posts.length ? (
-                    <div className='has-posts'>
-                        {this.props.posts.map(posts => (
-                            <Card
-                            title={posts.title} />
-                        ))}
-                    </div>
-                ) : (
-                        <div className='no-posts'>
-                            <p>Nothing here! Please enter a subreddit into the search bar to get that reddit's posts!</p>
-                        </div>
-                    )}
-            </SearchContainer>
-        );
+  handleSearch = event => {
+    event.preventDefault();
+    if (this.state.search) {
+      Snoo.snooScrape(this.state.search).then(res => {
+        const { postActions } = this.props;
+        postActions.postsToProps(res);
+      });
     }
+  };
+  render() {
+    return (
+      <SearchContainer>
+        <form>
+          <div className="search-form-container">
+            <p className="search-text">
+              Please Search for a subreddit to return its contents!
+            </p>
+            <div className="search-form">
+              <SearchForm id="search" onChange={this.handleInputChange} />
+            </div>
+            <button
+              className="search-button"
+              onSubmit={this.preventDefault}
+              onClick={this.handleSearch}
+            >
+              Search
+            </button>
+          </div>
+        </form>
+
+        {this.props.posts.length ? (
+          <div className="has-posts">
+            {this.props.posts.map(posts => {
+              switch (posts.postType) {
+                case "link":
+                  return <div className="link-post">{posts.postType}</div>;
+                case "img":
+                  return <div className="img-post">{posts.postType}</div>;
+                case "rich-video":
+                  return <div className="rich-post">{posts.postType}</div>;
+                case "hosted-video":
+                  return <div className="hosted-post">{posts.postType}</div>;
+                case "undefined":
+                  return <div className="undefined-post">{posts.postType}</div>;
+                case "self":
+                  return <div className="self-post">{posts.postType}</div>;
+              }
+            })}
+          </div>
+        ) : (
+          <div className="no-posts">
+            <p>
+              Nothing here! Please enter a subreddit into the search bar to get
+              that reddit's posts!
+            </p>
+          </div>
+        )}
+      </SearchContainer>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        posts: state.posts,
-    };
+  return {
+    posts: state.posts
+  };
 }
 function mapDispatchToProps(dispatch) {
-    return {
-        postActions: bindActionCreators(postActionCreators, dispatch),
-    };
+  return {
+    postActions: bindActionCreators(postActionCreators, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
